f0a90ff35c4cc14611b7697febcc02af89e19915
sugarfish <2475945868@qq.com>
2024-12-19 14:29:04 +0800

    print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -489,8 +489,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
 
 #ifdef LAB_PGTBL
 void
+vmprint_walk(pagetable_t pagetable, int level, uint64 mask) {
+  static char *level_prefix[] = {".. .. ..", ".. ..", ".."};
+
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      uint64 va = mask | (uint64)(i << PXSHIFT(level));
+      uint64 pa = PTE2PA(pte);
+      printf("%s%p: pte %p pa %p\n", 
+        level_prefix[level], (void*)(va), (void*)pte, (void*)pa);
+      if(level > 0) {
+        vmprint_walk((pagetable_t)(pa), level-1, va);
+      }
+    }
+  }
+}
+
+void
 vmprint(pagetable_t pagetable) {
   // your code here
+  printf("page table %p\n", pagetable);
+  vmprint_walk(pagetable, 2, 0);
 }
 #endif
 
